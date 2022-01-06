b2006eb50e25b5f950dc97ce62106d74d47af30a
hardhill <hardhill@mail.ru>
2022-01-06 09:29:13 +0800

    first commit

---
+++ HardHillChess/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HardHillChess
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("HardHill Chess ver. 1.0");
+            while (true)
+            {
+                var command = Console.In.ReadLine();
+                if (command == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
